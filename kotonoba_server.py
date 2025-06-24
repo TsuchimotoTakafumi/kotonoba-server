34105df09b0287b1c720e99494a1ee8a7e9850af
Tsuchimoto Takafumi <tsuchi0703@icloud.com>
2025-06-24 14:25:04 +0900

    initial commit

--- kotonoba_server.py
+++ kotonoba_server.py
@@ -24,4 +24,5 @@ def get_word_meaning():
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # ポート5001でFlaskを起動
+    app.run(debug=True, host='0.0.0.0', port=5001)
